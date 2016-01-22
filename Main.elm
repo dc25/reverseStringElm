a5297f1e33a514a7f53ad3216afc17e25bb1d2ac
Dave Compton <davecompton7@gmail.com>
2016-01-22 06:54:14 +0000

    simplified

--- Main.elm
+++ Main.elm
@@ -9,64 +9,22 @@ import Maybe exposing (withDefault)
 months : List String
 months= ["January ", "February ", "March ", "April ", "May ", "June ", "July ", "August ", "September ", "October ", "November ", "December "]
 
-isLeap : Int -> Bool
-isLeap year = (year % 400) == 0 || ( (year % 4) == 0 && (year % 100) /= 0 )
-
-daysInMonth : Int -> List Int
-daysInMonth year = 
-  let febDays = if isLeap(year) then 29 else 28
-  in [31, febDays, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
--- Sunday through Saturday are represented by 0 through 6.
-firstDayOfYear : Int -> Int
-firstDayOfYear year =
-   let firstYear = 1601
-       delta = year - firstYear
-       daysBeforeThisYear = 365  * delta + delta // 4 - delta // 100 + delta // 400
-       firstDayOf1601 = 1 -- Monday 
-   in (firstDayOf1601 + daysBeforeThisYear) % 7
-
 lastFridayOfMonth : Int -> List Int
 lastFridayOfMonth year =
-  let daysBeforeMonth -- number of days in year before this month
-      = List.scanl (+) 0 (daysInMonth year)
+  let isLeap = (year % 400) == 0 || ( (year % 4) == 0 && (year % 100) /= 0 )
 
-      -- number of days in year before next month
-      daysBeforeNextMonth 
-      = daysBeforeMonth
+      daysInMonth = [31, if isLeap then 29 else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+      y = year-1
+
+  in  daysInMonth
+      |> List.scanl (+) 0 
       |> List.tail
       |> Maybe.withDefault []
+      |> List.map ((+) (365*y + y//4 - y//100 + y//400))
+      |> List.map (\day -> (day-5) % 7) 
+      |> List.map2 (-) daysInMonth 
 
-      -- index of the first day of the last week of the month
-      -- The "index" of a day is how many days into the year that day is.
-      lastWeekIndex 
-      = daysBeforeNextMonth
-      |> List.map ((+) -7)
-
-      -- day of week of first day of the last week of the month 
-      firstDayOfLastWeekInMonth 
-      = lastWeekIndex 
-      |> List.map ((+) (firstDayOfYear year))
-      |> List.map (\dayIndex -> dayIndex % 7)
-
-      -- days until Friday from the first day of the last week of the month
-      fridayOffset -- 
-      = firstDayOfLastWeekInMonth 
-      |> List.map (\day -> (12-day) % 7)
-
-      -- index of the last friday of the month
-      indexOfLastFriday 
-      = List.map2 (+) lastWeekIndex fridayOffset
-
-      -- "zero offset date" of the last friday of the month
-      dateOfLastFriday
-      = List.map2 (-) indexOfLastFriday daysBeforeMonth
-
-      -- date of the last friday of the month.
-      lastFridays
-      = List.map ((+) 1) dateOfLastFriday
-
-  in lastFridays
 
 errString : String
 errString = "Only years after 1752 are valid."
