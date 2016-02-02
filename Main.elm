c8a86635381249085d1464c6dcd341720627c6b2
Dave Compton <davecompton7@gmail.com>
2016-02-02 21:25:02 +0000

    updated to be just reverse string

--- Main.elm
+++ Main.elm
@@ -3,48 +3,25 @@ import Html.Attributes exposing (placeholder, value, style)
 import Html.Events exposing (on, targetValue)
 import Signal exposing (Address)
 import StartApp.Simple exposing (start)
-import String exposing (toInt)
-import Maybe exposing (withDefault)
-import List exposing (map, map2)
-import List.Extra exposing (scanl1)
-
-lastFridays : Int -> List Int
-lastFridays year =
-  let isLeap = (year % 400) == 0 || ( (year % 4) == 0 && (year % 100) /= 0 )
-      daysInMonth = [31, if isLeap then 29 else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-      y = year-1
-  in scanl1 (+) daysInMonth
-     |> map2 (\len day -> len - (day + 2 + y + y//4 - y//100 + y//400) % 7) daysInMonth
-
-lastFridayStrings : String -> List String
-lastFridayStrings yearString = 
-  let months= ["January ", "February ", "March ", "April ", "May ", "June ", "July ", "August ", "September ", "October ", "November ", "December "]
-      errString = "Only years after 1752 are valid."
-  in case toInt yearString of 
-       Ok year -> 
-           if (year < 1753) 
-           then [errString] 
-           else lastFridays year
-                |> map2 (\m d -> m ++ toString d ++ ", " ++ toString year) months
-       Err _ -> 
-           [errString]
+import String exposing (reverse)
 
 main = start { model = "", view = view, update = update }
 
 update newStr oldStr = newStr
 
 view : Address String -> String -> Html
-view address yearString =
+view address forwards =
   div []
     ([ input
-        [ placeholder "Enter a year."
-        , value yearString
+        [ placeholder "Enter a string to reverse."
+        , value forwards
         , on "input" targetValue (Signal.message address)
         , myStyle
         ]
         []
-     ] ++ (lastFridayStrings yearString
-           |> map (\date -> div [ myStyle ] [ text date ]) ))
+     ] ++ 
+     [ div [ myStyle] [text (reverse forwards)]
+     ])
 
 myStyle : Attribute
 myStyle =
